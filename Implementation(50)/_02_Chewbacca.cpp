c2780274d586513668ef7ed233a13a5b8631f94b
Harpreet singh <harpreet162004@gmail.com>
2024-03-23 13:52:21 +0530

    chewbacca number

---
+++ Implementation(50)/_02_Chewbacca.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <climits> // For INT_MAX
+using namespace std;
+
+int main()
+{
+    long long  n;
+    cin >> n;
+
+    string num = to_string(n);
+    for (int i = 0; i < num.length(); i++)
+    {
+        int digit = num[i] - '0';
+        if (i==0 && digit == 9)
+            continue;
+
+       num[i] = min(digit, 9-digit + '0'); //Inverting the digit
+    }
+
+    cout<< num << endl;
+
+    return 0;
+}
